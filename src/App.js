104452856ced7cdc332b73640a140ef5f422f2cc
Augustcode99 <burakodabasi99@yahoo.com>
2023-05-06 17:23:42 +0300

    data now processed by map

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import data from "./data.js";
 function App() {
   return (
     <>
-      <h1>React</h1>
-      <Person
-        name="august"
-        image="https://cdn.pixabay.com/photo/2023/04/23/18/44/ai-generated-7946503_960_720.jpg"
-        phoneNo="000555000"
-      />
-      <Person
-        name="alexa"
-        image="https://cdn.pixabay.com/photo/2023/02/26/22/33/girl-7817118_960_720.jpg"
-        phoneNo="000111000"
-      />
-      <Person
-        name="dave"
-        image="https://cdn.pixabay.com/photo/2023/04/21/06/28/ai-generated-7941217_960_720.jpg"
-        phoneNo="000333000"
-      />
+      <h1>People</h1>
+      {data.map((person) => {
+        return (
+          <Person
+            name={person.name}
+            image={person.image}
+            phoneNo={person.phoneNo}
+          />
+        );
+      })}
     </>
   );
 }
